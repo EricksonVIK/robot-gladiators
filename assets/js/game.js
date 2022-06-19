853d25becf15e13b627bde1c10171b6dd522821c
Erikson <chris.s.erickson@gmail.com>
2022-06-19 06:37:16 -0400

    starting the day off

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! Java Script is running")
+// window.alert("This is an alert! Java Script is running")
+
+// this creates a function named "fight"
+// declared function
+function fight() {
+    window.alert("The fight has begun!");
+}
+// called function
+fight();
+
+var playerName = window.prompt("What is your robot's name?");
+
+window.alert(playerName);
