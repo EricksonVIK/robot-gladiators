09340ed127689e6570ff9290d66fe05be658fcd2
Erikson <chris.s.erickson@gmail.com>
2022-06-20 12:27:25 -0400

    while and for loops with breaks

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,64 +16,91 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName){
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!")
+    // repeat and execute as long as the enemy-robot is alive
+    while(playerHealth > 0 && enemyHealth > 0){
+        // Alert players that they are starting the round
+        // window.alert("Welcome to Robot Gladiators!")
 
-    // Prompt for player to decide to fight or skip
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-    
-    // if player choses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT" ){
-        // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
-        console.log (
-            playerName + " attacked " + enemyName + " ." + enemyName + " now has " + enemyHealth + " health remaining."
-        );
+        // Prompt for player to decide to fight or skip
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-        // check enemy's health
-        if (enemyHealth <= 0){
-            window.alert(enemyName + " has died!");
-        } else {
-            window.alert(enemyName + " still has " + enemyHealth + "health left.")
-        }
 
-        // remove player's health by subtracting the amout st in the enemyAttack
-        playerHealth = playerHealth - enemyAttack;
-        console.log(
-            enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + "health remaining."
-        );
-
-        // check player's health
-        if (playerHealth <=0){
-            window.alert(playerName + " has died!");
-        } else {
-            window.alert(playerName + " still has " + playerHealth + " health left.");
-        }
-
-        //  if player choses to skip
-    } else if (promptFight === "skip" || promptFight === "SKIP"){
+        if (promptFight === "skip" || promptFight === "SKIP"){
         // confirm the player want sto skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-        // if yes (true), leave fight
-        if (confirmSkip){
+            // if yes (true), leave fight
+            if (confirmSkip){
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 2;
-            console.log(playerName + " has $ " + playerMoney + " remaining.")
+            playerMoney = playerMoney - 10;
+            console.log(" playerMoney", playerMoney)
+            break;
+            }
         }
-        // if no (false, ask question again by running fight() again
-        else { 
-            fight()
-        }
+
+            
+            // if player choses to fight, then fight
+            // if (promptFight === "fight" || promptFight === "FIGHT" ){
+            //     // remove enemy's health by subtracting the amount set in the playerAttack variable
+        enemyHealth = enemyHealth - playerAttack;
+        console.log (
+            playerName + " attacked " + enemyName + " ." + enemyName + " now has " + enemyHealth + " health remaining.");
+            
+
+            // check enemy's health
+            if (enemyHealth <= 0){
+                window.alert(enemyName + " has died!");
+                // award player money for wining
+                playerMoney= playerMoney +20;
+                break;
+            } else {
+                window.alert(enemyName + " still has " + enemyHealth + " health left.")
+            }
+
+            // remove player's health by subtracting the amout st in the enemyAttack
+            playerHealth = playerHealth - enemyAttack;
+            console.log(
+                enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining."
+            );
+
+            // check player's health
+            if (playerHealth <=0){
+                window.alert(playerName + " has died!");
+                break;
+            } else {
+                window.alert(playerName + " still has " + playerHealth + " health left.");
+            }
+
+            //  if player choses to skip
+        // } else if (promptFight === "skip" || promptFight === "SKIP"){
+        //     // confirm the player want sto skip
+        //     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        //     // if yes (true), leave fight
+        //     if (confirmSkip){
+        //         window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        //         // subtract money from playerMoney for skipping
+        //         playerMoney = playerMoney - 10;
+        //         console.log(" playerMoney", playerMoney)
+        //         break;
+        //     }
+            // if no (false, ask question again by running fight() again
+            // else { 
+            //     fight()
+            // }
+        // turned to comment to remove fight option, skip only option, otherwise fight
+        // } else {
+        //     window.alert("You need to choose a valid option. Try again!");
+            // }
         
-    } else {
-        window.alert("You need to choose a valid option. Try again!");
-    }
-}    
+    }    
+}
 
 // fight() - Single round for function
 for(var i = 0; i < enemyNames.length; i++){
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50
+    fight(pickedEnemyName);
 }
 // Notice that the fight() function call was replaced with a for loop that calls the fight() function multiple times using the element in the enemyNames[i] array as the argument.
